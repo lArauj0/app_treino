3d30e84003bdd2c4bd65a3456514581508f2cbe7
Lucas Araújo <137833022+lArauj0@users.noreply.github.com>
2025-12-17 11:11:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+// sw.js - Service Worker simples
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('treino-store').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
